da5a8cffd317b2adde4e60798841ee9577cf15c2
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-08-19 09:11:13 +0200

    Modify application to verify auto commit on openapi.json

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     ...
 
 
+@app.post("/another")
+def another():
+    ...
+
+
+@app.get("/health", include_in_schema=False)
+def health():
+    ...
